edacc30301762f6f5fa6a3a4304a0266c8fbaeb9
German Molina <rgermanm@MBP-de-German.fibertel.com.ar>
2022-12-01 21:51:54 -0300

    gradient and backgrounds

--- web-puentes-next/pages/index.js
+++ web-puentes-next/pages/index.js
@@ -5,8 +5,8 @@ import Navbar from '../components/navbar'
 import Section from '../components/section'
 import BackgroundBanner from '../public/Assets/BackgroundBanner.png';
 import FotoBanner from '../public/Assets/FotoBanner.jpg'
-import BackgroundOferta from '../public/Assets/Background-OfertaAcademica-01.png'; 
-import BackgroundPrograma from '../public/Assets/Background-ElPrograma1-01.png'; 
+import BackgroundOferta from '../public/Assets/Background-OfertaAcademica-01.png';
+import BackgroundPrograma from '../public/Assets/Background-ElPrograma1-01.png';
 import BackgroundPrograma2 from '../public/Assets/Background-ElPrograma2-01.png';
 import gradientCodes from '../public/gradientCodes.json'
 export default function Home() {
@@ -20,17 +20,19 @@ export default function Home() {
       <Navbar></Navbar>
 
       <div className={styles.welcome}>
-        
+
         <div className={styles.backgroundBannerContainer}>
           <Image className={styles.backgroundBanner} src={BackgroundBanner} />
-          <Image className={styles.fotoBanner} src={FotoBanner}/>
+          <Image className={styles.fotoBanner} src={FotoBanner} />
 
         </div>
       </div>
-      <Section background={BackgroundOferta} ></Section>
-      <Section background={BackgroundPrograma} gradientCode={gradientCodes.programa}></Section>
-      <Section background={BackgroundPrograma2}></Section>
-      <Section gradientCode={gradientCodes.footer}></Section>
+      <Section id="Oferta" background={BackgroundOferta} ></Section>
+      <Section id="Programa" background={BackgroundPrograma} gradientCode={gradientCodes.programa}></Section>
+      <Section id="Programa2" background={BackgroundPrograma2}></Section>
+      <Section id="Galeria" gradientCode={gradientCodes.galeria}></Section>
+      <Section id="Universidades" gradientCode={gradientCodes.universidades}></Section>
+      <Section id="Footer" gradientCode={gradientCodes.footer}></Section>
       <footer className={styles.footer}>
 
       </footer>
