7b38703d93fdf33abc9dfeaf35fb0271929a6be2
Fernando Correa <fer.lopes.correa@gmail.com>
2023-03-15 15:59:30 -0300

    image changes

--- web-puentes-next/components/statsCard.js
+++ web-puentes-next/components/statsCard.js
@@ -2,11 +2,11 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import CountUp from "react-countup";
 
-export default function StatsCard({ ilustracion, fontColor, stat, label }) {
+export default function StatsCard({ ilustracion, fontColor, stat, label ,bigger}) {
   return (
     <div className="stats-card">
       <div>
-        <Image className="stats-illustration" style={{width:60}} src={ilustracion}></Image>
+        <Image className="stats-illustration" style={{width:bigger?120:60}} src={ilustracion}></Image>
       </div>
       <div className="stats-number-container">
         <div>
