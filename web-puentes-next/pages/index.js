4f5e44113700046ac9a7f837163a1e235195b715
German Molina <r.german.molina@gmail.com>
2023-03-16 13:41:15 -0300

    window resize fix

--- web-puentes-next/pages/index.js
+++ web-puentes-next/pages/index.js
@@ -67,6 +67,7 @@ import SectionGaleria from "../components/sectionGaleria";
 
 export default function Home() {
   const [actualPath, setactualPath] = useState("");
+  const size = useWindowSize();
 
   const stats = [
     {
@@ -480,7 +481,7 @@ export default function Home() {
           </Section>
         </div>
 
-        <SectionGaleria id="Galeria" customStyles={{scrollMarginTop:window.innerWidth<700?"120px":"0px"}} background={BackgroundGaleria}>
+        <SectionGaleria id="Galeria" customStyles={{scrollMarginTop:size.width<700?"120px":"0px"}} background={BackgroundGaleria}>
           <div
             style={{
               display: "flex",
@@ -604,4 +605,36 @@ export default function Home() {
       </div>
     </ChakraProvider>
   );
+}
+
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
